728be0676f7fd3248e4e8b23873bb72299ec7bfc
yhirochick <yhirochick@gmail.com>
2019-07-14 21:36:24 +0900

    aboutページ修正、post-form作成。とりあえずスタンダード目指す

--- src/app/chongshengde/chongshengde-routing.module.ts
+++ src/app/chongshengde/chongshengde-routing.module.ts
@@ -1,3 +1,5 @@
+import { PostFormComponent } from './post-form/post-form.component';
+import { PostedComponent } from './posted/posted.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -7,7 +9,8 @@ import { ChongshengdeComponent } from './chongshengde.component';
 const routes: Routes = [
     { path: '', component: ChongshengdeComponent,
         children:[
-            // { path: 'post', component: },
+            { path: 'post-form', component: PostFormComponent},
+            { path: 'posts', component: PostedComponent},
         ]
     }
 ];
